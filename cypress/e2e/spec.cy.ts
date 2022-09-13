d90fe643453f5f69003a42602b284a4a7044a921
Batmagnai Monkhnasan <magnai103019@gmail.com>
2022-09-13 13:48:38 +0800

    dev

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe("empty spec", () => {
   it("passes", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
     cy.get("h1").contains("BATMAGNAI");
 
     // The new page should contain an h1 with "About page"
